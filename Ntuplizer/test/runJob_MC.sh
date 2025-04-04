e4d2a0dca8a2224ab78c28d55662f109633aac36
Quibusque <cruck990@gmail.com>
2025-04-04 12:38:08 +0200

    Add job submission and execution scripts for MC and Cosmics processing for htcondor

---
+++ Ntuplizer/test/runJob_MC.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+# Make sure the script is executable: chmod +x runJob.sh
+
+cmsenv
+
+# Get command line arguments: input directory and output file name
+input_dir=$1
+out_file=$2
+
+# Define log file (this can be customized)
+logfile="log_MC_${input_dir}_MiniAOD.log"
+
+echo "Running cmsRun for input directory: ${input_dir}"
+start_time=$(date +%s)
+
+# Run cmsRun with your configuration file; adjust if needed
+cmsRun MC_MiniAOD_runNtuplizer_cfg_test.py -input_dir ${input_dir} -out_file ${out_file} &> ${logfile}
+
+end_time=$(date +%s)
+echo "Time taken: $((end_time - start_time)) seconds"
+
+# Copy the output file to the EOS output directory if it exists
+output_dir="/eos/home-m/mcrucian/SWAN_projects/DisplacedMuons/ntuples"
+if [ -f "${out_file}" ]; then
+    cp ${out_file} ${output_dir}
+    echo "${out_file} copied to ${output_dir}"
+else
+    echo "Error: ${out_file} not found."
+fi
---
+++ Ntuplizer/test/runJob_cosmics.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# Make sure the script is executable: chmod +x runJob_cosmics.sh
+
+cmsenv
+
+# Define log file (this can be customized)
+logfile="log_Cosmics_AOD.log"
+
+echo "Running cmsRun for Cosmics..."
+start_time=$(date +%s)
+
+# Run cmsRun with your configuration file
+cmsRun Cosmics_runNtuplizer_AOD_cfg.py &> ${logfile}
+
+end_time=$(date +%s)
+echo "Time taken: $((end_time - start_time)) seconds"
+
+# Copy the output file to the EOS output directory if it exists
+output_dir="/eos/home-m/mcrucian/SWAN_projects/DisplacedMuons/ntuples"
+if [ -f "ntuples.root" ]; then
+    cp ntuples.root ${output_dir}
+    echo "ntuples.root copied to ${output_dir}"
+else
+    echo "Error: ntuples.root not found."
+fi
